44ae45b819c3d1c59df09bccf1df06452f4fd3ba
EmThanh <trungthanhhy1@gmail.com>
2024-04-24 12:34:59 +0000

    Setting up experiments and test

--- source/train_cifar10.py
+++ source/train_cifar10.py
@@ -17,7 +17,6 @@ from tqdm import tqdm
 
 # Weights and Biases API
 import wandb
-import random
 
 train_config = {
     'model_type': 'VGG11',
@@ -28,8 +27,6 @@ train_config = {
     'num_epochs': 20,
 }
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
 # %% Setup the datasets
 transform_func = v2.Compose([
     v2.ToImage(), 
@@ -37,11 +34,8 @@ transform_func = v2.Compose([
 ])
 
 train_dataset = datasets.CIFAR10(root='../cifar10_dataset', train=True, download=True, transform=transform_func)
-
 test_dataset = datasets.CIFAR10(root='../cifar10_dataset', train=False, download=True, transform=transform_func)
-
 train_dataloader = torch.utils.data.DataLoader(train_dataset, shuffle = True, num_workers=16, batch_size = train_config['batch_size'], pin_memory=True, prefetch_factor=4)
-
 test_dataloader = torch.utils.data.DataLoader(test_dataset, shuffle = False, num_workers=16, batch_size = len(test_dataset))
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
@@ -57,36 +51,59 @@ wandb.init(
     "dataset": "CIFAR-10",
     "epochs": train_config['num_epochs'],
     },
-    name='test_run'
+    name='cifar10_test_run'
 )
+
 # %% Initiate models, criterion, optimizer and scheduler
 if train_config['model_type'] == 'VGG11':
     net = VGG('VGG11')
 elif train_config['model_type'] == 'Resnet18':
     net = ResNet18()
 
-net.to(device)
-cudnn.benchmark = True
-
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=train_config['lr'],
                       momentum=train_config['momentum'], weight_decay=train_config['weight_decay'])
 
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+# %% Accelerator
+accelerator = Accelerator()
+device = accelerator.device
+net, optimizer, train_dataloader = accelerator.prepare(net, optimizer, train_dataloader)
+
 # %% Training
 print('Total number of parameters: ', sum([p.numel() for p in net.parameters()]))
+net.train()
 for i in tqdm(range(train_config['num_epochs'])):
     train_loss = 0
+    correct = 0
     for inputs, targets in train_dataloader:
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
         outputs = net(inputs)
         loss = criterion(outputs, targets)
-        loss.backward()
+        accelerator.backward(loss)
         optimizer.step()
 
         train_loss += loss.item()
 
-    wandb.log({"train_loss": train_loss})
+        label_outputs = outputs.argmax(dim = 1)
+        correct += (label_outputs == targets).float().sum()
+
+    inputs_test, targets_test = next(iter(test_dataloader))
+    inputs_test, targets_test = inputs_test.to(device), targets_test.to(device)
+    outputs_test = net(inputs_test)
+    correct_test = (outputs.argmax(dim = 1) == targets_test).float().sum()
+    test_loss = criterion(outputs_test, targets_test)
+
+
+
+    train_accuracy = 100 * correct / len(train_dataset)
+    test_accuracy = 100 * correct_test / len(test_dataset)
+    wandb.log({
+        "train_loss": train_loss, 
+        "train_accuracy": train_accuracy, 
+        "test_loss": test_loss,
+        "test_accuracy": test_accuracy
+    })
 
 wandb.finish()
--- source/vgg.py
+++
@@ -1,2 +0,0 @@
-from models import *
-model = VGG('VGG11')
