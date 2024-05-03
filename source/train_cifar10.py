7455baf817cbacff691fd268c24882da17ec9212
EmThanh <trungthanhhy1@gmail.com>
2024-05-03 01:28:27 +0000

    Configure accelerate

--- source/train_cifar10.py
+++ source/train_cifar10.py
@@ -18,12 +18,31 @@ from tqdm import tqdm
 # Weights and Biases API
 import wandb
 
+import argparse
+# %% Argparse
+msg = "Script for running training on CIFAR-10 Dataset"
+parser = argparse.ArgumentParser(description = msg)
+parser.add_argument('-d', '--device', help = 'Choose a CUDA device to operate on (0 - num_device). Defaut: CUDA:0', default=0)
+parser.add_argument('-a', '--accelerate', action='store_true', help = 'Activate multi-GPU training')
+
+args = parser.parse_args()
+
+# %% Config of the run
+# train_config = {
+#     'model_type': 'VGG11',
+#     'lr': 0.1,
+#     'weight_decay': 1e-5,
+#     'batch_size': 128,
+#     'momentum': 0.9,
+#     'num_epochs': 20,
+# }
+
 train_config = {
     'model_type': 'VGG11',
     'lr': 0.1,
-    'weight_decay': 1e-5,
+    'weight_decay': 0,
     'batch_size': 128,
-    'momentum': 0.9,
+    'momentum': 0,
     'num_epochs': 20,
 }
 
@@ -36,23 +55,9 @@ transform_func = v2.Compose([
 train_dataset = datasets.CIFAR10(root='../cifar10_dataset', train=True, download=True, transform=transform_func)
 test_dataset = datasets.CIFAR10(root='../cifar10_dataset', train=False, download=True, transform=transform_func)
 train_dataloader = torch.utils.data.DataLoader(train_dataset, shuffle = True, num_workers=16, batch_size = train_config['batch_size'], pin_memory=True, prefetch_factor=4)
-test_dataloader = torch.utils.data.DataLoader(test_dataset, shuffle = False, num_workers=16, batch_size = len(test_dataset))
+test_dataloader = torch.utils.data.DataLoader(test_dataset, shuffle = False, num_workers=16, batch_size = train_config['batch_size'])
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-# %% Record on WANDB.AI
-wandb.init(
-    # set the wandb project where this run will be logged
-    project="cifar10-learning-dynamics",
-
-    # track hyperparameters and run metadata
-    config={
-    "learning_rate": train_config['lr'],
-    "architecture": train_config['model_type'],
-    "dataset": "CIFAR-10",
-    "epochs": train_config['num_epochs'],
-    },
-    name='cifar10_test_run'
-)
 
 # %% Initiate models, criterion, optimizer and scheduler
 if train_config['model_type'] == 'VGG11':
@@ -64,16 +69,36 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=train_config['lr'],
                       momentum=train_config['momentum'], weight_decay=train_config['weight_decay'])
 
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 # %% Accelerator
 accelerator = Accelerator()
 device = accelerator.device
 net, optimizer, train_dataloader = accelerator.prepare(net, optimizer, train_dataloader)
+# %% Record on WANDB.AI
+if accelerator.is_main_process:
+    run = wandb.init(
+        # set the wandb project where this run will be logged
+        project="cifar10-learning-dynamics",
 
+        # track hyperparameters and run metadata
+        config={
+        "learning_rate": train_config['lr'],
+        "architecture": train_config['model_type'],
+        "dataset": "CIFAR-10",
+        "epochs": train_config['num_epochs'],
+        },
+        name='cifar10_test_run'
+    )
+
+    run.define_metric("epoch")
+    run.define_metric("train_loss", step_metric="epoch")
+    run.define_metric("train_accuracy", step_metric="epoch")
+    run.define_metric("test_loss", step_metric="epoch")
+    run.define_metric("test_accuracy", step_metric="epoch")
 # %% Training
 print('Total number of parameters: ', sum([p.numel() for p in net.parameters()]))
-net.train()
+
 for i in tqdm(range(train_config['num_epochs'])):
+    net.train()
     train_loss = 0
     correct = 0
     for inputs, targets in train_dataloader:
@@ -88,22 +113,31 @@ for i in tqdm(range(train_config['num_epochs'])):
 
         label_outputs = outputs.argmax(dim = 1)
         correct += (label_outputs == targets).float().sum()
+        if accelerator.is_main_process:
+            run.log({
+                'train_loss_step': loss.item()
+            })
 
-    inputs_test, targets_test = next(iter(test_dataloader))
-    inputs_test, targets_test = inputs_test.to(device), targets_test.to(device)
-    outputs_test = net(inputs_test)
-    correct_test = (outputs.argmax(dim = 1) == targets_test).float().sum()
-    test_loss = criterion(outputs_test, targets_test)
-
+    net.eval()
+    test_loss = 0
+    correct_test = 0
+    with torch.no_grad():
+        for inputs_test, targets_test in test_dataloader:
+            inputs_test, targets_test = inputs_test.to(device), targets_test.to(device)
+            outputs_test = net(inputs_test)
+            correct_test += (outputs_test.argmax(dim = 1) == targets_test).float().sum()
+            test_loss += criterion(outputs_test, targets_test)
 
 
     train_accuracy = 100 * correct / len(train_dataset)
     test_accuracy = 100 * correct_test / len(test_dataset)
-    wandb.log({
-        "train_loss": train_loss, 
-        "train_accuracy": train_accuracy, 
-        "test_loss": test_loss,
-        "test_accuracy": test_accuracy
-    })
+    if accelerator.is_main_process:
+        run.log({
+            "epoch": i,
+            "train_loss": train_loss, 
+            "train_accuracy": train_accuracy, 
+            "test_loss": test_loss,
+            "test_accuracy": test_accuracy
+        })
 
 wandb.finish()
